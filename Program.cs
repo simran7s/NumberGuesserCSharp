01f7db9f603ecb2226ec9684705376eb1ed96a1e
simran7s <simran7s@my.yorku.ca>
2021-12-09 14:21:09 -0500

    completed hello world with comments

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using System; //importing other namespaces that can be used here
 
-namespace NumberGuesser
+
+namespace NumberGuesser //container for classes or functions
 {
-    class Program
+    class Program //used to create objects
     {
+
+        //entry point method
+
+        //static = using static lets all instances of a class share the same variables
+        //void = no return
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
